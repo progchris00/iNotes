2a9c1b85fe614167dccc23b5890c21224f35aa8e
progchris00 <prog.christian07@gmail.com>
2023-11-24 17:18:30 +0800

    Modified sql

--- inote/notes.py
+++ inote/notes.py
@@ -3,8 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from inote.auth import login_required
+from inote.db import get_db
 
 bp = Blueprint('notes', __name__)
 
@@ -12,8 +12,8 @@ bp = Blueprint('notes', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT n.id, title, body, created, author_id, username'
+        ' FROM note n JOIN user u ON n.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('notes/index.html', posts=posts)
